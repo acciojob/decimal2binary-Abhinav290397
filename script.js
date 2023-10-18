21f3f2b282d672bc9f8da59bceb07b25efca18a3
Abhinav Mathur <142848301+Abhinav290397@users.noreply.github.com>
2023-10-18 18:50:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 function decimalToBinary(num) {
   //Write you code here
-  
+	let temp = num;
+	let str = "";
+	while (temp != 1) {
+		let rem = temp%2;
+		str += rem;
+		temp = Math.floor(temp/2);
+	}
+	str += '1';
+    let ans = "";
+	for(let i=str.length-1;i>=0;i--){
+		ans += str.charAt(i);
+	}
+	return ans;
 }
 
 window.decimalToBinary = decimalToBinary;
